b52d34efd0a0dbe1839225f508263ccc9ed4ea56
paweljaworski <pawel.jaworski@intive.com>
2021-12-28 11:10:36 +0100

    second client

--- resource-server/src/main/java/pl/javorex/resourceserver/config/Oauth2ClientsConfig.java
+++ resource-server/src/main/java/pl/javorex/resourceserver/config/Oauth2ClientsConfig.java
@@ -15,7 +15,7 @@ class Oauth2ClientsConfig {
 
   @Bean
   ClientRegistrationRepository clientRegistrationRepository() {
-    return new InMemoryClientRegistrationRepository(client1());
+    return new InMemoryClientRegistrationRepository(client1(), client2());
   }
 
   ClientRegistration client1() {
@@ -34,13 +34,40 @@ class Oauth2ClientsConfig {
       .build();
   }
 
+  ClientRegistration client2() {
+    return ClientRegistration.withRegistrationId("client-2")
+      .issuerUri(Client2.issuerUri)
+      .clientId(Client2.clientId)
+      .clientSecret(Client2.clientSecret)
+      .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+      .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+      .redirectUri(Client2.redirectUri)
+      .scope(SCOPE_OPENID)
+      .clientName(Client2.clientName)
+      .authorizationUri(Client2.authorizationUri)
+      .tokenUri(Client2.tokenUri)
+      .jwkSetUri(Client2.jwksSetUri)
+      .build();
+  }
+
   // /.well-known/oauth-authorization-server
   // /.well-known/openid-configuration
   private static class Client1 {
     static String issuerUri = "http://localhost:9001";
     static String clientId = "articles-client";
     static String clientSecret = "secret";
-    static String clientName = clientId;
+    static String clientName = "first";
+    static String redirectUri = "http://127.0.0.1:8080/login/oauth2/code/{registrationId}";
+    static String authorizationUri = issuerUri + "/oauth2/authorize";
+    static String tokenUri = issuerUri + "/oauth2/token";
+    static String jwksSetUri = issuerUri + "/oauth2/jwks";
+  }
+
+  private static class Client2 {
+    static String issuerUri = "http://localhost:9002";
+    static String clientId = "articles-client";
+    static String clientSecret = "secret";
+    static String clientName = "second";
     static String redirectUri = "http://127.0.0.1:8080/login/oauth2/code/{registrationId}";
     static String authorizationUri = issuerUri + "/oauth2/authorize";
     static String tokenUri = issuerUri + "/oauth2/token";
